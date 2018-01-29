1d90fd4c4a23a3602b2120655a0ca8dafb1c9952
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-01-30 11:09:02 +1300

    Added example data

--- rxjs-chat/src/app/app.component.ts
+++ rxjs-chat/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import {ChatExampleData} from './data/chat-example-data';
 
 @Component({
   selector: 'app-root',
